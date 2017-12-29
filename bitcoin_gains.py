fa68b156c8c4deef656a3c856833b5466b948859
Sopho <sophom+gh@gmail.com>
2017-12-29 09:52:30 -0500

    Fix for merging wallets. Make sure we have lost.btc before we subtract them. See issue #4.

--- bitcoin_gains.py
+++ bitcoin_gains.py
@@ -1257,7 +1257,8 @@ def main(args):
                 if t.type == 'transfer':
                     if lost_in_transfer:
                         lost, buy = buy.split(lost_in_transfer)
-                        lost_in_transfer -= lost.btc
+                        if lost:
+                            lost_in_transfer -= lost.btc
                     if buy:
                         push_lot(t.dest_account, buy)
                         account_btc[t.dest_account] += buy.btc
